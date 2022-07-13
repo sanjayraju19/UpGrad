f502b57838c8097bd65078ceafd8ec471f7f4e4b
sanjayraju19 <sanjayraju19@gmail.com>
2022-07-13 17:57:24 +0530

    Hellow

---
+++ firstProgram/src/Main.java
@@ -0,0 +1,11 @@
+
+import com.sun.source.tree.AssertTree;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year = input.nextInt();
+    }
+}
